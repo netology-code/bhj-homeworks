335b10871817b2dd409eb147ef196f261bfd6605
Vladimir Yazykov <neizerth@gmail.com>
2019-02-18 02:33:41 +0300

    init & 1.1

---
+++ js-features/mole-game/base.js
@@ -0,0 +1,22 @@
+(() => {
+  let playing = true,
+    activeHole = 1;
+
+  const stop = () => playing = true,
+    getHole = index => document.getElementById(`hole${index}`),
+    deactivateHole = index =>
+      getHole( index ).className = 'hole',
+    activateHole = index =>
+      getHole( index ).className = 'hole hole_has-mole',
+    next = () => setTimeout(() => {
+      if ( !playing ) {
+        return;
+      }
+      deactivateHole( activeHole );
+      activeHole = Math.floor( 1 + Math.random() * 9 );
+      activateHole( activeHole );
+      next();
+    }, 800 );
+
+  next();
+})();
