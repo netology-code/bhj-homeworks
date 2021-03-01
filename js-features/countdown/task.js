f863f7615598e203cef92b836752729956af1756
KrisStrat <krisstrat@yandex.ru>
2021-03-01 16:58:28 +0200

    исправления по первому домашнему заданию

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,5 +1,9 @@
 let time = document.getElementById("timer");
 let count = function() {
   time.textContent -= 1;
+  if (time.textContent == 0) {
+    alert("Вы победили!")
+    clearInterval(timerId);
+  }
 };
-setInterval(count,1000);
+let timerId = setInterval(count,1000);
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,19 +1,20 @@
-let holes = document.getElementsByClassName("hole");
+let holes = document.querySelectorAll(".hole");
 let counterWin = document.getElementById("dead");
 let counterLose =document.getElementById("lost");
 
+console.log(holes);
 
 
-
-for (i = 1 ; i < holes.lenght ; i++) {
-  holes[i].onclick = function(e) {
-    if(holes[i].className.includes("hole_has-mole")) {
+for (i = 1 ; i < holes.length ; i++) {
+  let classNameHole = holes[i].className;
+  holes[i].onclick = function() {
+    if(classNameHole.includes("hole_has-mole")) {
       counterWin.textContent + 1;
       if(counterWin.textContent = 10) {
         alert("Вы победили!");
       }
       }
-    else {
+    if(classNameHole.includes("hole_has-mole")!= true) {
       counterLose.textContent + 1;
       if(counterLose.textContent = 5) {
         alert("Вы проиграли!");
