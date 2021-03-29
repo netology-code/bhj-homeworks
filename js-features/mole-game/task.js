c5d071b8bc290b437a454268d02f3bd4ce74ef40
KrisStrat <krisstrat@yandex.ru>
2021-03-29 17:30:48 +0300

    исправила,но все равно не понимаю почему код не работает.

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,6 +1,10 @@
 let holes = document.querySelectorAll(".hole");
 let counterWin = document.getElementById("dead");
 let counterLose =document.getElementById("lost");
+let numberWin = counterWin.textContent;
+let numberLose = counterLose.textContent;
+let newNumberWin = parseInt(numberWin);
+let newNumberLose = parseInt(numberLose);
 
 console.log(holes);
 
@@ -9,14 +13,16 @@ for (i = 1 ; i < holes.length ; i++) {
   let classNameHole = holes[i].className;
   holes[i].onclick = function() {
     if(classNameHole.includes("hole_has-mole")) {
-      counterWin.textContent + 1;
-      if(counterWin.textContent = 10) {
+      newNumberWin = newNumberWin + 1;
+      numberWin = newNumberWin;
+      if(numberWin == 10) {
         alert("Вы победили!");
       }
       }
     if(classNameHole.includes("hole_has-mole")!= true) {
-      counterLose.textContent + 1;
-      if(counterLose.textContent = 5) {
+      newNumberLose = newNumberLose + 1;
+      numberLose = newNumberLose;
+      if(numberLose == 5) {
         alert("Вы проиграли!");
       }
     }
