052f0c681204bd91629dea53bb9ccaaac57e0ce2
KrisStrat <krisstrat@yandex.ru>
2021-03-31 16:36:41 +0300

    ура!наконец то готово!

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,28 +1,22 @@
 let holes = document.querySelectorAll(".hole");
 let counterWin = document.getElementById("dead");
 let counterLose =document.getElementById("lost");
-let numberWin = counterWin.textContent;
-let numberLose = counterLose.textContent;
-let newNumberWin = parseInt(numberWin);
-let newNumberLose = parseInt(numberLose);
+
 
 console.log(holes);
 
 
 for (i = 1 ; i < holes.length ; i++) {
-  let classNameHole = holes[i].className;
   holes[i].onclick = function() {
-    if(classNameHole.includes("hole_has-mole")) {
-      newNumberWin = newNumberWin + 1;
-      numberWin = newNumberWin;
-      if(numberWin == 10) {
+    if(this.className.includes("hole_has-mole")) {
+      counterWin.textContent = +counterWin.textContent + 1;
+      if(counterWin.textContent == 10) {
         alert("Вы победили!");
       }
       }
-    if(classNameHole.includes("hole_has-mole")!= true) {
-      newNumberLose = newNumberLose + 1;
-      numberLose = newNumberLose;
-      if(numberLose == 5) {
+    if(this.className.includes("hole_has-mole")!= true) {
+      counterLose.textContent = +counterLose.textContent + 1;
+      if(counterLose.text == 5) {
         alert("Вы проиграли!");
       }
     }
