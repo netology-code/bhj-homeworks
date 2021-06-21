4d7f6052e242ff2aab947e2c8062c8945a18f827
Olga800 <72503619+Olga800@users.noreply.github.com>
2021-06-21 08:18:06 +0300

    Update task.js

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,22 +1,25 @@
 'use strict';
+let counter = 0;
+	let tabActive = 0;
+	let tabContentActive = 0;
+	const menu = document.body.querySelectorAll(".tab");
+	for (let i = 0; i < menu.length; i++) {
+	    menu[i].addEventListener("click", function () {
+	        this.classList.add("tab_active");
+	        const content = document.body.querySelectorAll('.tab__content');
+	        if (counter >= 1) {
+	            tabActive.remove("tab_active");
+	            tabContentActive.remove("tab__content_active");
+	            if (counter > 3) {
+	                counter = 0;
+	                tabContentActive = 0;
+	            };
+	
+	        };
+	        content[i].classList.add('tab__content_active');
+	        tabContentActive = content[i].classList;
+	        tabActive = this.classList;
+	        counter += 1;
+	    });
+	};
 
-const tabs = document.getElementsByClassName('tab');
-const tabContents = document.getElementsByClassName('tab__content');
-
-let tabIndex = 0;
-
-const selectTab = newIndex => {
-  if(tabIndex === newIndex);
-    return;
-  tabs[tabIndex].classList.remove('tab_active');
-  tabContents[tabIndex].classList.remove('tab__content_active');
-  tabs[newIndex].classList.add('tab_active');
-  tabContents[newIndex].classList.add('tab__content_active');
-  tabIndex = newIndex;
-};
-for (let i = 0; i < tabs.length; i++) {
-  tabs[i].index = i;
-  tabs[i].addEventListener('click', function() {
-    selectTab(this.index);
-  });
-};
