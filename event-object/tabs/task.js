4791ba89c5f8ba12b6361df887ab2c8ffae15170
KrisStrat <62306057+KrisStrat@users.noreply.github.com>
2021-11-26 21:58:57 +0200

    Add files via upload
    не могу понять в чем причина, консоль ошибок не выдает, думаю как отменить или словить предыдущий таргет,что бы снять класс active.

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,14 +1,32 @@
-let tabs = arrayFrom.(document.querySelectorAll(".tab"));
-let subTabs = arrayFrom(document.querySelectorAll(".tab__contents"));
+let tabs = Array.from(document.querySelectorAll(".tab"));
+let contents = Array.from(document.querySelectorAll(".tab__content"));
 
+//tabs.forEach((active) => {
+  //active.addEventListener('click', (event) => {
+    //if ( active.classList.contains(".tab_active")) {
+      //active.classList.remove(".tab_active");
+    //}else {
+      //active.classList.add(".tab_active");
+    //};
+    //let indexOfCont = tabs.indexOf(active);
+    //if (contents[indexOfCont].classList.contains(".tab__content_active")) {
+      //contents[indexOfCont].classList.remove(".tab__content_active");
+    //} else {
+      //contents[indexOfCont].classList.add(".tab__content_active");
+    //};
+    //event.preventDefault();
+  //});
+//});
 
-function changeStatus () {
-
-  for (i=0, i < tabs.length ,i++) {
-    tabs[i].classList.add("tab_active")
-  };
-    if (tabs[i].classList.contains(".tab_active") ) {
-      subTabs[i].classList.add(".tab__content_active");
-    };
-};
-tabs.addEventListener('click', changeStatus);
+for (i=0; i<tabs.length; i++) {
+  tabs[i].addEventListener('click', (event) => {
+    //console.log(event.currentTarget.closest("tab_active"));
+    event.currentTarget.classList.add("tab_active");
+    console.log(event.currentTarget.classList);
+    let indexCont = tabs.indexOf(event.currentTarget);
+    console.log(indexCont);
+    tabs[indexCont].classList.add("tab__content_active");
+    console.log(tabs[indexCont].classList);
+    event.preventDefault();
+  });
+}
