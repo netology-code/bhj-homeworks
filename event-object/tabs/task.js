4b8752efe843fe86292769b8f8d7e4cca5a3bf81
Juliya Trifonova <yula_sakura@list.ru>
2022-02-06 20:13:44 +0300

    Initial commit

---
+++ event-object/tabs/task.js
@@ -0,0 +1,9 @@
+const tab = document.getElementByClassName('tab');
+const tab__content = document.getElementByClassName('tab__content');
+
+tab.addEventListener(click, function() {
+    tab.className = 'tab_active';
+});
+tab__contentitem(indexOf(arr)).addEventListener(click, function() {
+    tab__contentitem(indexOf(arr)).className = 'tab__content_active';
+});
