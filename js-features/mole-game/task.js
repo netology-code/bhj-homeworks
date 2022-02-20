2f3e509ad494fa13eaf9dd7842235abc2667492c
Juliya Trifonova <yula_sakura@list.ru>
2022-02-20 22:11:05 +0300

    Initial commit

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,14 +1,22 @@
-for(let i = 1; i++; i < 10) {
-    const hole = document.getElementById('hole' + i);
+const dead = document.getElementById('dead');
+const lost = document.getElementById('lost');
+
+let index = Math.floor( 1 + Math.random() * 9 )
+getHole = index => hole = document.getElementById('hole' + index);
+
+    if(getHole(index).className.includes('hole_has-mole')) {
+    getHole(index).onclick = function() {
+    dead.textContent += 1;
+    }
+    getHole(!index).onclick = function() {
+    lost.textContent += 1;
+    }
+    if(lost.textContent === 5) {
+        alert('Поражение!');
+    }
+    else if(dead.textContent === 10) {
+        alert('Победа!');
+    }
 }
 
-if(hole.className.includes( 'hole_has-mole' )) {
-    const dead = document.getElementById('dead');
-    dead.onclick = function() {
-    dead += 1;
-    }
-    const lost = document.getElementById('lost');
-    lost.onclick = function() {
-    lost += 1;
-    }
 }
