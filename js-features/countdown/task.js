90bd3cfe96137af50a38f3ece52db311f9195020
Juliya Trifonova <yula_sakura@list.ru>
2022-02-20 22:11:59 +0300

    Initial commit

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,6 +1,19 @@
 const timer = document.getElementById('timer');
+const url = document.createElement('a');
 const output = function() {
-    timer += 1;
+    const timerToString = timer.textContent -= 1;
+    timerToString.toString({
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+    });
 }
+
 setInterval(output, 1000);
-alert('Вы победили в конкурсе!');
+
+if(timerToString === 0) {
+    clearInterval(output);
+    alert('Вы победили в конкурсе!');
+    url.href = 'http://hello.kitty';
+};
+
