7b82a5eaa47536af12effdc1f416dfdfc857225a
Juliya Trifonova <yula_sakura@list.ru>
2022-02-21 23:47:41 +0300

    Initial commit

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,12 +1,18 @@
 const rotator__case = document.getElementsByClassName('rotator__case');
 rotator__case.className = 'rotator__case_active';
 
-function toggleClass() {
-    rotator__case.classList.toggle('rotator__case_active');
-}
-
-for(const element of rotator__case) {
+const arr = Array.from(rotator__case);
+for(let i = 0; i++; i < arr.length) {
+    function toggleClass() {
+        arr[i - 1].classList.remove('rotator__case_active');
+        arr[i].classList.add('rotator__case_active');
+    }
+    if(i === arr.length - 1) {
+        i = 0;
+    }
     setInterval(toggleClass, 1000);
 }
 
-
+const span = document.querySelector('span');
+console.log(span.dataset.speed);
+console.log(span.dataset.color);
