81b709f9ff844dcab64bec2ce63289283114e7d2
Juliya Trifonova <yula_sakura@list.ru>
2022-02-22 00:34:09 +0300

    Initial commit

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -2,12 +2,17 @@ const modal_main = document.getElementById('modal_main');
 modal_main.className = 'modal_active';
 
 const modal__close = document.getElementsByClassName('modal__close');
-modal__close.onclick = function() {
-    modal_main.className = 'modal';
+for(const close of modal__close) {
+    close.onclick = function() {
+        modal_main.classList.add('modal');
+    }
 }
 
 const show_success = document.getElementsByClassName('show-success');
 const modal_success = document.getElementsByClassName('modal_success');
-show_success.onclick = function() {
-    modal_success.className = 'modal_active';
+for(const show of show_success) {
+    show.onclick = function() {
+        for(const success of modal_success)
+        success.classList.add('modal_active');
+    }
 }
