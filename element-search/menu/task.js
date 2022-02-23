b79bdd06df65b53b74653510230cc52ac34fad40
Juliya Trifonova <yula_sakura@list.ru>
2022-02-23 15:50:55 +0300

    Initial commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,9 +1,25 @@
-const menu__link = document.getElements.ByClassName('menu__link');
-menu__link.onclick = function() {
-    menu__link.closest('ul').className = 'menu_active';
+const menu__link = document.getElementsByClassName('menu__link');
+const a = document.querySelectorAll('a');
+
+let arr_menu__link = Array.from(menu__link);
+for(let i = 0; i < arr_menu__link.length; i++) {
+    arr_menu__link[i].onclick = function() {
+        if(arr_menu__link[i].nextElementSibling.classList.contains('menu_sub')) {
+            arr_menu__link[i].nextElementSibling.classList.add('menu_active');
+            let arr_filter = arr_menu__link.filter((elem) => elem !== arr_menu__link[i]);
+            for(let elem of arr_filter) {
+                elem.classList.remove('menu_active');
+            }
+        }
+    }
 }
 
-const menu_sub = document.getElements.ByClassName('menu_sub');
-menu_sub.onclick = function() {
-    return false;
+let a_arr = Array.from(a);
+for(let elem of a) {
+    elem.onclick = function() {
+        if(elem.closest.classList.contains('menu__item')) {
+            return false;
+        }
+    }
 }
+
