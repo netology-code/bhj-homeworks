4149b034ac745b4c1eabdb70974784e8282b4f10
Juliya Trifonova <yula_sakura@list.ru>
2022-02-25 00:08:55 +0300

    Initial commit

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,18 +1,20 @@
-const task = document.getElementsByClassName('task');
 const tasks__list = document.getElementsByClassName('tasks__list');
-
-for (const element of tasks__list) {
-    tasks__list.addEventListener('KeyboardEvent.enterKey', insert_text())
-    function insert_text() {
-        tasks__list.insertAdjacentText('afterBegin', task.innerText)
-    }
-}
-
 const task__remove = document.getElementsByClassName('task__remove');
-for (const element of task__remove) {
-    task__remove.addEventListener('click', remove_text());
-    function remove_text() {
-        tasks__list.removeChild(task.innerText)
-    }
 
+const list_arr = Array.from(tasks__list);
+const remove_arr = Array.from(task__remove);
+
+for (let elem_list of list_arr) {
+    elem_list.addEventListener('KeyboardEvent.enterKey', insert_text())
+    function insert_text() {
+        elem_list.insertAdjacentText('afterBegin', task.innerText)
+    }
+    for (let elem_remove of remove_arr) {
+        elem_remove.addEventListener('click', remove_text());
+        function remove_text() {
+            elem_list.removeChild(task.innerText)
+        }
+    
+    }
 }
+
