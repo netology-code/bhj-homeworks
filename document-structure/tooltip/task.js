4e7f5a8ff5eeb91e95b4413cae41a3005359c802
Juliya Trifonova <yula_sakura@list.ru>
2022-02-25 00:09:56 +0300

    Initial commit

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,6 +1,6 @@
 const helpers = document.getElementsByClassName('has-tooltip');
-
-for (const helper of helpers) {
+const arr_helpers = Array.from(helpers);
+for (let helper of arr_helpers) {
     function helper_show() {
         helper.classList.add('tooltip_active');
         console.log(helper.getAttribute('title'));
