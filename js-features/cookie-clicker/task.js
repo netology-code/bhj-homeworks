be7c57287f8dd24768f24a447404216a83fb5437
Juliya Trifonova <yula_sakura@list.ru>
2022-03-07 16:32:00 +0300

    Commit

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,13 +1,13 @@
 const clicker__counter = document.getElementById('clicker__counter');
 const cookie = document.getElementById('cookie');
-let clicker_timer = Number(clicker__counter.textContent);
-
 const clicker__velocity = document.getElementsByClassName('clicker__velocity');
+
+let clicker_timer = Number(clicker__counter.textContent);
 let time_1 = Number(new Date());
 let velocity = Number(clicker__velocity.textContent);
 let interval = 0;
+
 cookie.onclick = function() {
-    this.clicker_timer = clicker_timer;
     clicker_timer++;
     if(clicker_timer % 2 === 0) {
         cookie.width += 100;
@@ -16,8 +16,8 @@ cookie.onclick = function() {
         cookie.width -= 100;
     }
     let time_2 = Number(new Date());
-    interval = time_2 - time_1;
-    velocity = interval/clicker_timer;
+    interval === time_2 - time_1;
+    velocity === interval/clicker_timer;
 }
 
 
