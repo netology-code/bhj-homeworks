01a2d23000450f5e37aa6b182f629ed325a06175
Juliya Trifonova <yula_sakura@list.ru>
2022-03-07 17:25:44 +0300

    Commit

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,24 +1,23 @@
 const dropdown__list = document.getElementsByClassName('dropdown__list');
 const dropdown__item = document.getElementsByClassName('dropdown__item');
 const dropdown__link = document.getElementsByClassName('dropdown__link');
+const dropdown__value = document.getElementsByClassName('dropdown__value');
 
 const arr_item = Array.from(dropdown__item);
 const arr_list = Array.from(dropdown__list);
+const arr_link = Array.from(dropdown__link);
+const arr_value = Array.from(dropdown__value);
+
 
 for(let list of arr_list) {
-    list.addEventListener(click, function() {
-        list.classList.add('dropdown__list_active');
+    for(let link of arr_link) {
+    link.addEventListener('click', function() {
+        list.classList.toggle('dropdown__list_active');
+        link.preventDefault();
+        for(let value of arr_value) {
+            value.textContent = link.textContent;
+
+        }
     })
 }
-
-for(item of arr_item) {
-    item.addEventListener(click, function() {
-        arr_list.find((elem) => elem.classList.contains('dropdown__list_active')).remove('dropdown__list_active');
-        item.classList.add('dropdown__value');
-    });
 }
-
-function preventDefault() {
-    dropdown__link.preventDefault();
-}
-dropdown__link.addEventListener(click, preventDefault);
