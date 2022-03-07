cf1aa8dbdb3d48e76995c982ab4209e2cce0234b
Juliya Trifonova <yula_sakura@list.ru>
2022-03-07 23:43:28 +0300

    Commit

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,25 +1,31 @@
+
+
+
+
+
+
+
+
 const product__quantity_controls = document.getElementsByClassName('product__quantity-controls');
-const cart__product = document.getElementsByClassName('cart__product');
+const cart = document.getElementById('1');
 const cart__product_count = document.getElementsByClassName('cart__product-count');
 
 let products_arr = Array.from(product__quantity_controls);
-let cart_arr = Array.from(cart__product);
 
 for(let product of products_arr) {
-    for(let cart of cart_arr) {
         product.children[2].addEventListener('click', function() {
-            if(parseInt(cart.lastElementChild.textContent) > 0) {
-                product.children[1].textContent++;
+            if(Number(cart.lastElementChild.textContent) > 0) {
+                Number(product.children[1].textContent)++;
             }
-            else if(parseInt(cart.lastElementChild.textContent) === 0) {
-                product.children[1].textContent++;
-                parseInt(cart.lastElementChild.textContent) === product.children[1];
+            else if(Number(cart.lastElementChild.textContent) === 0) {
+                Number(product.children[1].textContent)++;
+                Number(cart.lastElementChild.textContent) === product.children[1];
             }
         });
         product.children[0].addEventListener('click', function() {
-            if(parseInt(cart.lastElementChild.textContent) > 1) {
-                pproduct.children[1].textContent--;
+            if(Number(cart.lastElementChild.textContent) > 1) {
+                Number(product.children[1].textContent)--;
             }
         });
-    }
+    
 }
