c237291c040fb1bfbcc92be1d9dbcf520abf374d
Juliya Trifonova <yula_sakura@list.ru>
2022-03-08 19:01:17 +0300

    Commit

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,42 +1,25 @@
+const slider__arrows = document.getElementsByClassName('slider__arrows');
 const slider__item = document.getElementsByClassName('slider__item');
-const slider__arrow_prev = document.getElementsByClassName('slider__arrow_prev');
-const slider__arrow_next = document.getElementsByClassName('slider__arrow_next');
-const slider__dot = document.getElementsByClassName('slider__dot');
 
-let arr = Array.from(slider__item);
-let slider_arr = Array.from(slider__dot);
+let arr_arrows = Array.from(slider__arrows);
+let arr_item = Array.from(slider__item);
 
-for(let elem of arr) {
-    let i = arr.findIndex(elem.classList.contains('slider__item_active'));
-    slider__arrow_prev.onclick = function() {
-        if(i === 0) {
-            i === arr.length - 1;
+for(let arrow of slider__arrows) {
+for(let item of arr_item) {
+    arrow.firstChild.addEventListener('click', function() {
+        item.classList.remove('slider__item_active');
+        item.previousElementSibling.classList.add('slider__item_active');
+        if(arr_item.indexOf(item) === 1) {
+            item[arr_item.length - 1].classList.add('slider__item_active');
         }
-        arr[i].classList.remove('slider__item_active');
-        arr[i-1].classList.add('slider__item_active');
-    }
-    slider__arrow_next.onclick = function() {
-        if(i === arr.length - 1) {
-            i === 0;
+    });
+    arrow.lastChild.addEventListener('click', function() {
+        item.classList.remove('slider__item_active');
+        item.nextElementSibling.classList.add('slider__item_active');
+        if(arr_item.indexOf(item) === arr_item.length - 1) {
+            item[0].classList.add('slider__item_active');
         }
-        arr[i].classList.remove('slider__item_active');
-        arr[i+1].classList.add('slider__item_active');
-    }
+    });
 }
-
-    for (let slide of slider_arr) {
-        let index = slider_arr.findIndex(slide.classList.contains('slider__dot_active'));
-
-        let arr_not_active = slider_arr.filter((slider_not_active) => slider_not_active !== slide[index]);
-        slide.onclick = function() {
-            slide.classList.add('slider__dot_active');
-            arr[index].classList.add('slider__item_active');
-            arr_not_active.every((elem) => elem.classList.remove('slider__dot_active'));
-        }
-        let items_not_active = arr.filter((item_not_active) => item_not_active !== arr[index]);
-        items_not_active.every((item) => item.classList.remove('slider__item_active'));
-    }
-
-
-
+}
 
