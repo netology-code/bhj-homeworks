269af8dad5d3537e73f2c50ab09b8d43765d9240
Juliya Trifonova <yula_sakura@list.ru>
2022-03-14 00:32:35 +0300

    Commit

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -3,19 +3,16 @@ const book = document.getElementById("book");
 const content = document.querySelector(".book__content");
 
 for (const elem of a) {
-elem.addEventListener("click", function (event) {
-event.preventDefault();
-console.log(event.target); // можно посмотреть что там за элемент
-elem.classList.add("font-size_active");
-if (event.target.classList.contains("font-size_big")) {
-content.classList.add("book", "bookfs-big");
-} else if (event.target.classList.contains("font-size_small")) {
-content.classList.add("book_fs-small");
-}
-else if(elem !== event.target) {
-    content.className = 'book';
-    elem.classList.remove('font-size_active');
-
-}
-});
+    elem.addEventListener("click", function (event) {
+        event.preventDefault();
+        console.log(event.target);
+        elem.classList.add("font-size_active");
+        if (event.target.classList.contains("font-size_big")) {
+            content.classList.add("book", "bookfs-big");
+        } else if (event.target.classList.contains("font-size_small")) {
+            content.classList.add("book_fs-small");
+        }
+        a[!a.indexOf(elem)].className = 'book';
+        a[!a.indexOf(elem)].classList.remove('font-size_active');
+    });
 }
