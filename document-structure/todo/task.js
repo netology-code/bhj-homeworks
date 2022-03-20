701ca5d741dbe4232f03628ec495e2e105d0a8e9
Juliya Trifonova <yula_sakura@list.ru>
2022-03-20 22:20:00 +0300

    Commit

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,22 +1,18 @@
-const tasks__list = document.getElementsByClassName('tasks__list');
-const task__remove = document.getElementsByClassName('task__remove');
+const tasks__list = document.querySelector('.tasks__list');
 const task__input = document.getElementById('task__input');
 
 const list_arr = Array.from(tasks__list);
-const remove_arr = Array.from(task__remove);
 
-for (let elem_list of list_arr) {
-    if(task__input.textContent) {
-        elem_list.addEventListener('keyup', insert_text());
-        function insert_text() {
-            elem_list.insertAdjacentText('afterBegin', task.innerText);
-        }
-        for (let elem_remove of remove_arr) {
-            elem_remove.addEventListener('click', remove_text());
-            function remove_text() {
-                elem_list.removeChild(task.innerText)
-            }
-        }
+if(task__input.textContent) {
+    task__input.addEventListener('submit', function() {
+    tasks__list.insertAdjacentHTML('afterBegin', tasks__list.firstChild.outerHTML);
+    tasks__list.insertAdjacentText('afterBegin', tasks__list.firstChild.innerText = task__input.textContent);
+    });
+    for(let elem of tasks__list) {
+        elem.queryselector('.task__title').addEventListener('click', function(e) {
+            elem.remove();
+            e.preventDefault();
+        });
     }
 }
 
