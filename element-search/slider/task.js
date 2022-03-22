ee0f016054f2bcd85ea5802ebdd12dc148e4f582
Juliya Trifonova <yula_sakura@list.ru>
2022-03-22 18:44:10 +0300

    Commit

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,31 +1,31 @@
 const slider__item = document.getElementsByClassName('slider__item');
 const slider__arrow = document.getElementsByClassName('slider__arrow');
-//const slider__arrow_prev = document.getElementsByClassName('slider__arrow_prev');
-//const slider__arrow_next = document.getElementsByClassName('slider__arrow_next');
+const slider__arrow_prev = document.getElementsByClassName('slider__arrow_prev')[0];
+const slider__arrow_next = document.getElementsByClassName('slider__arrow_next')[0];
 
 let arr_item = Array.from(slider__item);
 let arr_arrow = Array.from(slider__arrow);
 
-let index_item = arr_item.findIndex(slide => slide.classList.contains('slider__item_active'));
+let index_item = 0;
 let item = arr_item[index_item];
 
-let index_arrow_prev = slider__arrow.findIndex(arrow => arrow.classList.contains('slider__arrow_prev'));
-let arrow_prev = slider__arrow[index_arrow_prev];
-
-let index_arrow_next = slider__arrow.findIndex(arrow => arrow.classList.contains('slider__arrow_next'));
-let arrow_next = slider__arrow[index_arrow_next];
-
-arrow_prev.addEventListener('click', function() {
+slider__arrow_prev.addEventListener('click', function() {
+    index_item = arr_item.findIndex(slide => slide.classList.contains('slider__item_active'));
     item.classList.remove('slider__item_active');
-    item.previousElementSibling.classList.add('slider__item_active');
-    if(arr_item.indexOf(item) === 1) {
+    if(item.previousElementSibling) {
+        item.previousElementSibling.classList.add('slider__item_active');
+    }
+    else if(!item.previousElementSibling) {
         item[arr_item.length - 1].classList.add('slider__item_active');
     }
 });
-arrow_next.addEventListener('click', function() {
+slider__arrow_next.addEventListener('click', function() {
+    index_item = arr_item.findIndex(slide => slide.classList.contains('slider__item_active'));
     item.classList.remove('slider__item_active');
-    item.nextElementSibling.classList.add('slider__item_active');
-    if(arr_item.indexOf(item) === arr_item.length - 1) {
+    if(item.nextElementSibling) {
+        item.nextElementSibling.classList.add('slider__item_active');
+    }
+    else if(!item.nextElementSibling) {
         item[0].classList.add('slider__item_active');
     }
 });
