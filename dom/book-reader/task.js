3b0c2a9e31340d42655f755e95486386afc86d93
Juliya Trifonova <yula_sakura@list.ru>
2022-03-23 17:56:47 +0300

    Commit

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,9 +1,14 @@
-const a = document.querySelectorAll("a");
+const font_size = document.querySelectorAll(".font-size");
+
 const book = document.getElementById("book");
 const content = document.querySelector(".book__content");
+const book__control = document.querySelector(".book__control");
 
-for (const elem of a) {
-    elem.addEventListener("click", function (event) {
+
+const font_size_arr = Array.from(font_size);
+
+for (font of font_size_arr) {
+    font.addEventListener("click", function (event) {
         event.preventDefault();
         console.log(event.target);
         elem.classList.add("font-size_active");
@@ -12,7 +17,8 @@ for (const elem of a) {
         } else if (event.target.classList.contains("font-size_small")) {
             content.classList.add("book_fs-small");
         }
-        a[!a.indexOf(elem)].className = 'book';
-        a[!a.indexOf(elem)].classList.remove('font-size_active');
+        const index = font_size_arr.findIndex(link => link !== elem);
+        font_size_arr[index].className = 'book';
+        font_size_arr[index].classList.remove('font-size_active');
     });
 }
