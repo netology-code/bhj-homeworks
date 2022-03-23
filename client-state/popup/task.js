7c0c25fc26dd30794ac85649a946874d91fa841d
Juliya Trifonova <yula_sakura@list.ru>
2022-03-23 12:48:50 +0300

    Commit

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,14 +1,11 @@
-const modal__close = document.getElementsByClassName('modal__close');
+const modal__close = document.querySelector('.modal__close');
 const subscribe_modal = document.getElementById('subscribe-modal');
 
-const arr_close = Array.from(modal__close);
 
-for(const elem of arr_close) {
-    elem.addEventListener('clik', function() {
+modal__close.addEventListener('click', function() {
         subscribe_modal.classList.remove('modal_active');
-        document.cookie = 'close=' + encodeURIComponent('Окно закрыто') + 'Expires = Mon, 01 March 2023 00:00:00 GMT;';
+        document.cookie = 'close=' + 'Окно закрыто';
     });
-}
 const getCookie = (close) => {
     const value = '; ' + document.cookie;
     let parts = value.split('; ' + close + '=');
@@ -16,7 +13,8 @@ const getCookie = (close) => {
         return parts
         .pop().split(';').shift();
     }
-    else if (!document.cookie.close) {
-        subscribe_modal.classList.add('modal_active');
-    }
+}
+
+if (!getCookie(close)) {
+    subscribe_modal.classList.add('modal_active');
 }
