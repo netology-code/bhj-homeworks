ffcf3d43a534fa5286b116b4e8d3a922368914ef
Juliya Trifonova <yula_sakura@list.ru>
2022-03-23 17:55:53 +0300

    Commit

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,13 +1,12 @@
 const rotator__case = document.getElementsByClassName('rotator__case');
-
 const arr = Array.from(rotator__case);
-for(let i = 0; i++; i < arr.length) {
-    function toggleClass() {
-        arr[i].classList.remove('rotator__case_active');
-        arr[i + 1].classList.add('rotator__case_active');
+const index = 0;
+
+function toggleClass() {
+    if(index === arr.length - 1) {
+        index = 0;
     }
-    if(i === arr.length - 1) {
-        i = 0;
-    }
-    setInterval(toggleClass, 1000);
+    arr[index].classList.remove('rotator__case_active');
+    arr[index + 1].classList.add('rotator__case_active');
 }
+setInterval(toggleClass, 1000);
