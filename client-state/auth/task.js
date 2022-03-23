dfb529023ded34c1caabb178a1bb8eac067d852f
Juliya Trifonova <yula_sakura@list.ru>
2022-03-23 12:48:16 +0300

    Commit

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -5,20 +5,28 @@ const welcome = document.getElementById('welcome');
 
 signin.classList.add('signin_active');
 
-signin__btn.addEventListener('click', function() {
+signin__btn.addEventListener('click', function(e) {
+    const formData = new FormData(signin);
     const xhr = new XMLHttpRequest();
-    xhr.send('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
-    if (xhr.responseText.success) {
-        welcome.classList.add('welcome_active');
-        localStorage.setItem('id', 'xhr.responseText.user_id');
-        welcome.textContent.innerHTML = localStorage.id;
-        console.log(welcome.textContent);
-    }
-    else if(!xhr.responseText.success) {
-        alert('Неверный логин/пароль');
-    }
-    else if (localStorage.getItem('id')) {
-        welcome.textContent.innerHTML = localStorage.id;
-        console.log(welcome.textContent);
-    }
-})
+    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
+    xhr.send(formData);
+    formData.reset();
+
+    xhr.addEventListener('load', function() {
+        if (xhr.responseText.success) {
+            welcome.classList.add('welcome_active');
+            localStorage.setItem('id', 'xhr.responseText.user_id');
+            welcome.textContent.innerHTML = localStorage.id;
+            console.log(welcome.textContent);
+        }
+        else if(!xhr.responseText.success) {
+            alert('Неверный логин/пароль');
+        }
+        else if (localStorage.getItem('id')) {
+            welcome.textContent.innerHTML = localStorage.id;
+            console.log(welcome.textContent);
+        }
+    })
+    e.preventDefault();
+});
+
