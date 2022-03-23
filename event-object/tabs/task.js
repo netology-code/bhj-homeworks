1e9a465a93b48c020fcd114138dd5854a144e4e7
Juliya Trifonova <yula_sakura@list.ru>
2022-03-23 23:19:30 +0300

    Commit

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -4,11 +4,18 @@ const tab__content = document.getElementsByClassName('tab__content');
 const tab_arr = Array.from(tab);
 const content_arr = Array.from(tab__content);
 
-for(let tab_elem of tab_arr) {
+for(const tab_elem of tab_arr) {
     tab_elem.addEventListener('click', function() {
         tab_elem.classList.add('tab_active');
         let index = tab_arr.indexOf(tab_elem);
         tab__content[index].classList.add('tab__content_active');
+        for(const tab_active of tab_arr) {
+            if(tab_active !== tab_elem) {
+                tab_active.classList.remove('tab_active');
+                let index_active = tab_arr.indexOf(tab_active);
+                tab__content[index_active].classList.remove('tab__content_active');
+            }
+        }
     });
 }
 
