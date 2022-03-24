004d0574c6ce8b9513d5f419f1a052d2289d69bc
Juliya Trifonova <yula_sakura@list.ru>
2022-03-25 01:36:39 +0300

    Commit

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -4,12 +4,15 @@ const task__input = document.getElementById('task__input');
 const list_arr = Array.from(tasks__list);
 
 if(task__input.textContent) {
-    task__input.addEventListener('submit', function() {
-    tasks__list.insertAdjacentHTML('afterBegin', tasks__list.firstChild.outerHTML);
-    tasks__list.insertAdjacentText('afterBegin', tasks__list.firstChild.innerText = task__input.textContent);
+    task__input.addEventListener('submit', function(e) {
+        if(task__input.checkValidity()) {
+            tasks__list.insertAdjacentHTML('afterBegin', tasks__list.firstChild.outerHTML);
+            tasks__list.insertAdjacentText('afterBegin', tasks__list.firstChild.innerText = task__input.textContent);
+            e.preventDefault();
+        }
     });
     for(let elem of tasks__list) {
-        elem.queryselector('.task__title').addEventListener('click', function(e) {
+        elem.queryselector('.task__remove').addEventListener('click', function(e) {
             elem.remove();
             e.preventDefault();
         });
