dace5a3dd2696834c13e74fa25af2e68a9765bd3
Juliya Trifonova <yula_sakura@list.ru>
2022-03-26 17:02:25 +0300

    Commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -4,16 +4,13 @@ let arr_sub = Array.from(menu_sub);
 
 for(let sub of arr_sub) {
     const link = sub.closest('li').querySelector('a');
-    const openedSubMenu = arr_sub.find(elem => elem.classList.contains('menu_active'));
         link.onclick = function() {
-            if(openedSubMenu) {
+            const openedSubMenu = arr_sub.find(elem => elem.classList.contains('menu_active'));
+            if(openedSubMenu && openedSubMenu !== sub) {
                 openedSubMenu.classList.remove('menu_active');
-                return false;
             }
-            else if(openedSubMenu !== sub) {
-                sub.classList.add('menu_active');
-                return false;
-            }
+            sub.classList.toggle('menu_active');
+            return false;
         }
 }
 
