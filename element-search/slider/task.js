809e038013aef6c7f5ab7578fd07c3c3f4798cd3
Juliya Trifonova <yula_sakura@list.ru>
2022-03-26 17:04:07 +0300

    Commit

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -6,26 +6,23 @@ const slider__arrow_next = document.getElementsByClassName('slider__arrow_next')
 let arr_item = Array.from(slider__item);
 let arr_arrow = Array.from(slider__arrow);
 
-let index_item = 0;
-let item = arr_item[index_item];
-
 slider__arrow_prev.addEventListener('click', function() {
-    index_item = arr_item.findIndex(slide => slide.classList.contains('slider__item_active'));
+    const index_item = arr_item.findIndex(slide=>slide.classList.contains('slider__item_active'));
+    const item = arr_item[index_item];
     item.classList.remove('slider__item_active');
-    if(item.previousElementSibling) {
+    if (item.previousElementSibling) {
         item.previousElementSibling.classList.add('slider__item_active');
-    }
-    else if(!item.previousElementSibling) {
-        item[arr_item.length - 1].classList.add('slider__item_active');
+    } else if (!item.previousElementSibling) {
+        arr_item[arr_item.length - 1].classList.add('slider__item_active');
     }
 });
 slider__arrow_next.addEventListener('click', function() {
-    index_item = arr_item.findIndex(slide => slide.classList.contains('slider__item_active'));
+    const index_item = arr_item.findIndex(slide=>slide.classList.contains('slider__item_active'));
+    const item = arr_item[index_item];
     item.classList.remove('slider__item_active');
-    if(item.nextElementSibling) {
+    if (item.nextElementSibling) {
         item.nextElementSibling.classList.add('slider__item_active');
-    }
-    else if(!item.nextElementSibling) {
-        item[0].classList.add('slider__item_active');
+    } else if (!item.nextElementSibling) {
+        arr_item[0].classList.add('slider__item_active');
     }
 });
