55fb89664e8d158e8b22f87921dc3fe3a9ffb9e7
Juliya Trifonova <yula_sakura@list.ru>
2022-03-26 20:24:22 +0300

    Commit

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -3,18 +3,14 @@ const dropdown__list = document.querySelector('.dropdown__list');
 const dropdown__item = document.getElementsByClassName('dropdown__item');
 const dropdown__value = document.querySelector('.dropdown__value');
 
-const arr_item = Array.from(dropdown__item);
-
 dropdown.addEventListener('click', function() {
-    dropdown__list.classList.add('dropdown__list_active');
-});
-
-if(dropdown__list.classList.contains('dropdown__list_active')) {
-    for(const item of arr_item) {
+    dropdown__list.classList.toggle('dropdown__list_active');
+    for(const item of Array.from(dropdown__item)) {
         item.addEventListener('click', function(e) {
+            dropdown__value.textContent = item.querySelector('.dropdown__link').textContent;
             dropdown__list.classList.remove('dropdown__list_active');
-            dropdown__value.textContent === item.querySelector('.dropdown__link').textContent;
             e.preventDefault();
         })
     }
-}
+});
+
