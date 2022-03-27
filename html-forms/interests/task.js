4bf7bc3d8f6433bf66b7c2d55d583717b4342cbd
Juliya Trifonova <yula_sakura@list.ru>
2022-03-27 16:22:51 +0300

    Commit

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,9 +1,19 @@
-const interest = document.getElementsByClassName('interest');
-const interests = document.getElementsByClassName('interests');
+const interests_main = document.querySelector('.interests_main');
+const interests = interests_main.querySelector('ul').children;
 
-function selected() {
-    if(interest.selectedIndex) {
-        interest.children.options = true;
-    }
+for (const interest of Array.from(interests)) {
+
+    const interest__check = interest.querySelector('.interest__check');
+    const interests_active = interest.querySelector('.interests_active');
+    const check_active = Array.from(interests_active.querySelectorAll('.interest__check'));
+
+    interest__check.addEventListener('change', function() {
+        for (let active of check_active) {
+            if (interest__check.checked) {
+                active.checked = true;
+            } else {
+                active.checked = false;
+            }
+        }
+    });
 }
-interest.addEventListener('click', selected);
