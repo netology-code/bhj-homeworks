8131d65600be4979760364bd05af9709ea8ec522
Juliya Trifonova <yula_sakura@list.ru>
2022-03-27 16:22:26 +0300

    Commit

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,28 +1,34 @@
-const chat_widget = document.getElementsByClassName('chat-widget');
+const chat_widget = document.querySelector('.chat-widget');
+const chat_input = document.querySelector('.chat-widget__input');
 const message_client = document.getElementsByClassName('message_client');
-const messages = document.querySelector( '.chat-widget__messages' );
+const messages = document.getElementById('chat-widget__messages');
 
-
-let arr_widget = Array.from(chat_widget);
 let arr_client = Array.from(message_client);
 
-for(let widget of arr_widget) {
-    widget.addEventListener('click', function() {
-        widget.classList.add('chat-widget_active');    
-    });
-    widget.addEventListener('submit', function() {
-        for(let message of arr_client) {
-            if(message.lastChild.textContent) {
-                messages.innerHTML += `
-                <div class="message">
-                <div class="message__time">09:21</div>
-                <div class="message__text">
-                Добрый день, мы ещё не проснулись. Позвоните через 10 лет
-                </div>
-                </div>`;
-            }
-            message.firstChild.textContent = Date();
-        }
-    });
-}
+//  input.oninput = function() {
+//     document.getElementById('result').innerHTML = input.value;
+//   };
 
+
+chat_widget.addEventListener('click', function() {
+    chat_widget.classList.add('chat-widget_active');
+});
+
+chat_input.addEventListener('input', function(e) {
+
+    chat_widget.addEventListener('Enter', function(e) {
+
+    messages.innerHTML += `
+    <div class="message message_client">
+    <div class="message__time">` + new Date() + `</div>
+    <div class="message__text">` + chat_input.value + `</div>
+    </div>`;
+    
+    messages.innerHTML += `
+    <div class="message">
+    <div class="message__time">` + new Date() + `</div>
+    <div class="message__text">Добрый день, мы ещё не проснулись. Позвоните через 10 лет</div>
+    </div>`;
+});
+e.preventDefault();
+});
