ec00b81dccd9676e88fe5505ff6af6dff007f221
Juliya Trifonova <yula_sakura@list.ru>
2022-03-29 01:58:29 +0300

    Commit

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,54 +1,50 @@
+const products = document.querySelectorAll('.product');
 const cart__products = document.querySelector('.cart__products');
-const products = document.getElementsByClassName('product');
+const new_cart__product = document.createElement('div');
+        new_cart__product.innerHTML = 
+        `<div class="cart__product" data-id="1">
+             <img class="cart__product-image" src="image.png">
+             <div class="cart__product-count">20</div>
+         </div>`;
 
-const cart__product_count = document.getElementsByClassName('cart__product-count');
-
-let products_arr = Array.from(products);
-
-for(let product of products_arr) {
-
-    const cart = cart__products.querySelectorAll('.cart__product');
-    const cart_elem_arr = Array.from(cart);
-    const cart_elem = cart_elem_arr[cart_elem_arr.findIndex(elem => elem.getAttribute('data-id') === product.getAttribute('data-id'))]
-    
-    product.querySelector('.product__quantity-control_inc').addEventListener('click', function(e) {
-        if (cart_elem) {
-            Number(cart_elem.querySelector('.cart__product-count').textContent)++;
-        }
-        else if (!cart_elem) {
-            cart__products.insertAdjacentHTML('afterbegin', cart__products.firstElementChild.outerHTML);
-            cart_elem = cart__products.firstElementChild;
-            cart_elem.setAttribute('data-id') === product.getAttribute('data-id');
-            cart_elem.querySelector('.cart__product-image').setAttribute('src') === product.querySelector('.product__image').getAttribute('src');
-            Number(cart_elem.querySelector('.cart__product-count').textContent)++;
-        }
-        e.preventDefault();
-    });
-
+for(let product of Array.from(products)) {
+    let product__quantity_value = +(product.querySelector('.product__quantity-value').textContent);
     product.querySelector('.product__quantity-control_dec').addEventListener('click', function(e) {
-        if (cart_elem) {
-            if (Number(cart_elem.querySelector('.cart__product-count').textContent) == 1) {
-                cart_elem.remove();
-            }
-            else if (Number(cart_elem.querySelector('.cart__product-count').textContent) > 1) {
-                Number(cart_elem.querySelector('.cart__product-count').textContent)--;
-            }
+        
+        if(product__quantity_value !== 1) {
+            product__quantity_value--;
         }
         e.preventDefault();
     });
+    product.querySelector('.product__quantity-control_inc').addEventListener('click', function(e) {
+            product__quantity_value++;
+            e.preventDefault();
+    });
+    product.querySelector('.product__quantity-value').textContent = product__quantity_value;
 
-    product.querySelector('.product__add').addEventListener('click', function(e) {
-        if (cart_elem) {
-            Number(cart_elem.querySelector('.cart__product-count').textContent)++;
+    const product__add = product.querySelector('.product__add');
+    product__add.addEventListener('click', function(e) {
+        const cart__product = new_cart__product.cloneNode(true);
+        cart__product.getAttribute('data-id') === product.getAttribute('data-id');
+        cart__product.querySelector('.cart__product-image').getAttribute('src') === product.querySelector('.product__image').getAttribute('src');
+        cart__product.querySelector('.cart__product-count').textContent = product__quantity_value;
+       
+        const cart__products_arr = Array.from(cart__products.querySelectorAll('.cart__product'));
+        if(!cart__products_arr.includes(elem => elem.getAttribute('data-id') === product.getAttribute('data-id'))) {
+            cart__products.appendChild(cart__product);
         }
-        else if (!cart_elem) {
-            cart__products.insertAdjacentHTML('afterbegin', cart__products.firstElementChild.outerHTML);
-            cart_elem = cart__products.firstElementChild;
-            cart_elem.setAttribute('data-id') === product.getAttribute('data-id');
-            cart_elem.querySelector('.cart__product-image').setAttribute('src') === product.querySelector('.product__image').getAttribute('src');
-            Number(cart_elem.querySelector('.cart__product-count').textContent)++;
+        else {
+            const cart__product_added = cart__products_arr[cart__products_arr.findlndex(elem => elem.getAttribute('data-id') === product.getAttribute('data-id'))];
+            let cart__product_count = +(cart__product_added.querySelector('.cart__product-count').textContent);
+            cart__product_count++;
+            cart__product_added.querySelector('.cart__product-count').textContent = cart__product_count;
         }
+
+        
         e.preventDefault();
-    });
+    })
+
 }
+
+
 
