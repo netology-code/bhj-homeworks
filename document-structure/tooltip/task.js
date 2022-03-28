d324711de877386c5f4f80fe8c0ef39fe92380a3
Juliya Trifonova <yula_sakura@list.ru>
2022-03-29 01:59:35 +0300

    Commit

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,22 +1,23 @@
 const has_tooltip = document.getElementsByClassName('has-tooltip');
 const arr_tooltip = Array.from(has_tooltip);
 
+
 for (let elem of arr_tooltip) {
-    const tooltip = elem.querySelector('.tooltip');
     elem.addEventListener('click', function(e) {
-        if (tooltip) {
-            tooltip.textContent = elem.getAttribute('title');
-            tooltip.classList.toggle('tooltip_active');
-            console.log(tooltip.textContent);
-            e.preventDefault();
+
+        const tooltip = document.createElement('div');
+
+        tooltip.textContent = elem.getAttribute('title');
+        tooltip.classList.add('tooltip');
+
+        const tooltip_active = document.querySelector('.tooltip_active');
+
+        if(tooltip_active) {
+            tooltip_active.classList.remove('tooltip_active');
         }
-        else if (!tooltip) {
-            tooltip = elem.nextElementSibling;
-            tooltip.innerHTML = '<div class="tooltip"></div>';
-            tooltip.textContent = elem.getAttribute('title');
-            tooltip.classList.toggle('tooltip_active');
-            console.log(tooltip.textContent);
-            e.preventDefault();
-        }
+        
+        elem.append(tooltip);
+        tooltip.classList.add('tooltip_active');
+        e.preventDefault();
     });
 }
