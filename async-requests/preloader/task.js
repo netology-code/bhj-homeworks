e705e75e3371f7fd5420be752e4d17bb2a160e65
Juliya Trifonova <yula_sakura@list.ru>
2022-03-31 16:56:40 +0300

    Commit

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,31 +1,46 @@
-const items = document.getElementById('items');
-const loader = document.getElementsByClassName('loader');
-const item__code = document.getElementsByClassName('item__code');
-const item__value = document.getElementsByClassName('item__value');
+const items = document.querySelector("#items");
+const loader = document.querySelector(".loader");
+const itemCode = document.querySelectorAll(".item__code");
+const itemValue = document.querySelectorAll(".item__value");
 const xhr = new XMLHttpRequest();
+const newCurrency = document.createElement('div');
 
-let code_arr = Array.from(item__code);
-let value_arr = Array.from(item__value);
+newCurrency.innerHTML = `<div class="item__code">USD</div>
+     <div class="item__value">32</div>
+     <div class="item__currency">руб.</div>`
 
-xhr.open('GET', 'https://netology-slow-rest.herokuapp.com');
-xhr.addEventListener('progress', function() {
+let codeArr = Array.from(itemCode);
+let valueArr = Array.from(itemValue);
 
-    if(xhr.readyState === xhr.OPENED) {
-        items.children.innerHTML = xhr.responseText;
-        for(let elem of code_arr) {
-            elem.innerHTML = JSON.parse(xhr.responseText).Valute.AUD.CharCode;
-            elem.innerHTML = JSON.parse(xhr.responseText).Valute.AZN.CharCode;
+/*
+const formData = new FormData();
+for (const code of codeArr) {
+    formData.append('code', 'code.textContent');
+}
+for (const value of valueArr) {
+    formData.append('value', 'value.textContent');
+}
+xhr.send(formData);
+*/
+
+xhr.open("GET", "https://netology-slow-rest.herokuapp.com");
+
+xhr.addEventListener("readystatechange", function() {
+    if (xhr.readyState === xhr.DONE) {
+        const responseObject = JSON.parse(xhr.responseText);
+        console.log(responseObject);
+
+        const responseElemArr = Object.values(responseObject.response.Valute);
+        console.log(responseElemArr);
+        const currency = newCurrency.cloneNode(true);
+        items.appendChild(currency);
+        currency.classList.add('item');
+        currency.querySelector('.item__code').textContent = responseElemArr[2].CharCode;
+        currency.querySelector('.item__value').textContent = responseElemArr[2].Value;
+        for (let i = 0; i < responseElemArr.lengh - 1; i++) {
         }
-    
-        for(let elem of value_arr) {
-            elem.innerHTML = JSON.parse(xhr.responseText).Valute.AUD.Value;
-            elem.innerHTML = JSON.parse(xhr.responseText).Valute.AZN.Value;
-        }
-    }
-
-    else if(xhr.readyState === xhr.DONE) {
         loader.classList.remove('loader_active');
     }
-
 });
+
 xhr.send();
