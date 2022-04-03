bab690a6908c965098aa7a88e412767267dda4a4
Juliya Trifonova <yula_sakura@list.ru>
2022-04-03 23:53:39 +0300

    Commit

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,32 +1,39 @@
 const signin = document.getElementById('signin');
 const signin__btn = document.getElementById('signin__btn');
 const welcome = document.getElementById('welcome');
-
+const userId = document.getElementById('user_id');
+const signinForm = document.getElementById('signin__form');
 
 signin.classList.add('signin_active');
 
+if (localStorage.getItem('id')) {
+    userId.textContent = localStorage.id;
+    welcome.classList.add('welcome_active');
+}
+
 signin__btn.addEventListener('click', function(e) {
-    const formData = new FormData(signin);
+    const formData = new FormData(signinForm);
     const xhr = new XMLHttpRequest();
+
     xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
     xhr.send(formData);
-    formData.reset();
+    signinForm.reset();
 
-    xhr.addEventListener('load', function() {
-        if (xhr.responseText.success) {
-            welcome.classList.add('welcome_active');
-            localStorage.setItem('id', 'xhr.responseText.user_id');
-            welcome.textContent.innerHTML = localStorage.id;
-            console.log(welcome.textContent);
+    const responseObject = JSON.parse(xhr.responseText);
+
+    xhr.addEventListener('readystatechangereadystatechange', function() {
+        if (xhr.readyState === xhr.DONE) {
+            if (responseObject.success) {
+                
+                localStorage.setItem('id', 'responseObject.user_id');
+                userId.textContent = localStorage.id;
+                welcome.classList.add('welcome_active');
+            } 
+            
+            else if (!responseObject.success) {
+                alert('Неверный логин/пароль');
+            }
         }
-        else if(!xhr.responseText.success) {
-            alert('Неверный логин/пароль');
-        }
-        else if (localStorage.getItem('id')) {
-            welcome.textContent.innerHTML = localStorage.id;
-            console.log(welcome.textContent);
-        }
-    })
+    });
     e.preventDefault();
 });
-
