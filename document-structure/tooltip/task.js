30121d9e925e3668b40c0b72d61b06dc0c0d12de
Juliya Trifonova <yula_sakura@list.ru>
2022-04-07 18:00:22 +0300

    commit

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -6,15 +6,23 @@ for (let elem of arr_tooltip) {
     elem.addEventListener('click', function(e) {
 
         const tooltip = document.createElement('div');
-
         tooltip.textContent = elem.getAttribute('title');
         tooltip.classList.add('tooltip');
+        
+        const elemLeft = elem.getBoundingClientRect().left;
+        tooltip.style.left = elemLeft + 'px';
 
+        
         const tooltip_active = document.querySelector('.tooltip_active');
-
-        if(tooltip_active) {
+        if((tooltip_active)) {
             tooltip_active.classList.remove('tooltip_active');
         }
+
+        else if(tooltip.textContent.includes(elem.getAttribute('title'))) {
+            tooltip.classList.remove('tooltip_active');
+        }
+
+        
         
         elem.append(tooltip);
         tooltip.classList.add('tooltip_active');
