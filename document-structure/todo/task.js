21354ee25f0d4934aab6dd4161fa156d93ceebaa
Juliya Trifonova <yula_sakura@list.ru>
2022-04-09 17:44:11 +0300

    Commit

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,22 +3,27 @@ const tasks__form = document.getElementById('tasks__form');
 const tasks__list = document.getElementById('tasks__list');
 const task = document.querySelector('.task');
 const task_added = task.cloneNode(true);
+task.remove();
 
-task__input.addEventListener('input', function(e) {
-    if (task__input.value) {
-        tasks__form.addEventListener('submit', function(e) {
-            task_added.querySelector('.task__title').textContent = task__input.value;
-            tasks__list.appendChild(task_added);
-            e.preventDefault();
-        });
+
+tasks__form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+task_added.querySelector('.task__title').textContent = task__input.value;
+
+    if (task__input.value.trim()) {
+
+        tasks__list.appendChild(task_added);
         const task_next = task_added.cloneNode(true);
         task_added.replaceWith(task_next);
-    }
-    e.preventDefault();
-});
+        task__input.value = '';
 
-const task__remove = document.querySelector('.task__remove');
-task__remove.addEventListener('click', function(e) {
-    task__remove.parentElement.remove();
-    e.preventDefault();
+        const task__remove_arr = Array.from(document.querySelectorAll('.task__remove'));
+        for (const task__remove of task__remove_arr) {
+            task__remove.addEventListener('click', function(e) {
+                task__remove.parentElement.remove();
+                e.preventDefault();
+            });
+        }
+    }
 });
