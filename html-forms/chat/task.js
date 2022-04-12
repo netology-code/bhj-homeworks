4642b905d5a752631d0ee582dbd165efb0bebb3b
Juliya Trifonova <yula_sakura@list.ru>
2022-04-12 13:04:36 +0300

    Commit

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,34 +1,32 @@
 const chat_widget = document.querySelector('.chat-widget');
-const chat_input = document.querySelector('.chat-widget__input');
-const message_client = document.getElementsByClassName('message_client');
+const chat_widget_area = document.querySelector('.chat-widget__area');
+const chat_input = document.getElementById('chat-widget__input');
 const messages = document.getElementById('chat-widget__messages');
-
-let arr_client = Array.from(message_client);
-
-//  input.oninput = function() {
-//     document.getElementById('result').innerHTML = input.value;
-//   };
-
 
 chat_widget.addEventListener('click', function() {
     chat_widget.classList.add('chat-widget_active');
 });
 
-chat_input.addEventListener('input', function(e) {
-
-    chat_widget.addEventListener('Enter', function(e) {
-
-    messages.innerHTML += `
-    <div class="message message_client">
-    <div class="message__time">` + new Date() + `</div>
-    <div class="message__text">` + chat_input.value + `</div>
-    </div>`;
-    
-    messages.innerHTML += `
-    <div class="message">
-    <div class="message__time">` + new Date() + `</div>
-    <div class="message__text">Добрый день, мы ещё не проснулись. Позвоните через 10 лет</div>
-    </div>`;
-});
-e.preventDefault();
+chat_widget_area.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (chat_input.value.trim()) {
+        const message_client = document.createElement('div');
+        const message_robot = document.createElement('div');
+        
+        message_client.innerHTML = 
+          `<div class="message message_client">
+             <div class="message__time">` + new Date() + `</div>
+             <div class="message__text">` + chat_input.value + `</div>
+           </div>`;
+        
+        message_robot.innerHTML = 
+          `<div class="message"
+             <div class="message__time">` + new Date() + `</div>
+             <div class="message__text">Добрый день, мы ещё не проснулись. Позвоните через 10 лет</div>
+           </div>`;
+        
+        messages.appendChild(message_client);
+        messages.appendChild(message_robot);
+        chat_input.value = '';
+    }
 });
