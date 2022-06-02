a0a1dcaf8d22fe399d0993d1f3ec1fad1979d8dd
”Dmitry <dmitry.golofast@gmail.com>
2022-06-02 14:22:16 +0300

    fixed task 1.0

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,9 +4,13 @@ tooltip.setAttribute('class', 'tooltip');
 document.body.appendChild(tooltip);
 
 hasTooltips.forEach(tip => tip.addEventListener('click', function (e) {
+    if (tip.title === tooltip.textContent) {
+        tooltip.classList.toggle('tooltip_active');
+    } else {
+        tooltip.textContent = tip.getAttribute('title')
+        tooltip.classList.add('tooltip_active');
+    }
     e.preventDefault();
-    tooltip.classList.add('tooltip_active');
-    tooltip.innerText = tip.getAttribute('title');
     let place = tip.getBoundingClientRect();
     tooltip.style.left = place.left + "px";
     tooltip.style.top = place.top + 25 + "px";
