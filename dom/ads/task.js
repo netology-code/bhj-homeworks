8fcb2998deaef34736dbe1e8d191d9285646dfcd
sirius2206 <sergei_galkin@inbox.ru>
2022-06-13 16:18:35 +0500

    Homework 4 DOM

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -2,7 +2,7 @@ const rotator = document.querySelector('.rotator');
 let rotatorActive = rotator.querySelector('.rotator__case_active');
 let speed = rotatorActive.dataset.speed;
 rotatorActive.style.color = rotatorActive.dataset.color;
-    setTimeout(function next(speed){
+    setTimeout(function next(){
         rotatorActive.classList.remove('rotator__case_active');
         rotatorActive = rotatorActive.nextElementSibling;
         if (rotatorActive === null) {
