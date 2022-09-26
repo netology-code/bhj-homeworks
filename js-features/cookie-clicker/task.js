c5e50da75381c3e0de97bb0238992062fca9933c
Анастасия Ельшина <salnaya92@gmail.com>
2022-09-26 12:49:10 +0300

    just committed

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,5 +1,6 @@
 const clickCookie = document.getElementById('cookie');
 let countClick = 0;
+let timeFirstClick = Date.now();
 
 clickCookie.onclick = function() {
     countClick++;
@@ -8,6 +9,9 @@ clickCookie.onclick = function() {
     } else {
         clickCookie.width = 200;
     }
-    
     document.getElementById('clicker__counter').textContent = countClick;
+    let timeNextClick = Date.now();
+    const avgClick = 1/((timeNextClick - timeFirstClick)/1000);
+    document.getElementById('clicker_speed').textContent = avgClick.toFixed(2);
+    timeFirstClick = timeNextClick;
 }
--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -5,5 +5,31 @@ const countdownTimer = setInterval(() => {
     if (timer < 0) {
         clearInterval(countdownTimer);
         alert('Вы победили в конкурсе!')
+        document.getElementById('download').click();
     }
 }, 1000)
+
+const endTime = new Date('2022-09-20 18:30');
+let timerId = null;
+let hours = document.getElementById('hours');
+let minutes = document.getElementById('minutes');
+let seconds = document.getElementById('seconds');
+
+function countdownExtendedTimer() {
+    const differTime = endTime - new Date();
+    if (differTime <= 0) {
+        clearInterval(timerId);
+    }
+
+    const countHours = differTime > 0 ? Math.floor(differTime / 1000 / 60 / 60) % 24 : 0;
+    const countMinutes = differTime > 0 ? Math.floor(differTime / 1000 / 60) % 60 : 0;
+    const countSeconds = differTime > 0 ? Math.floor(differTime / 1000) % 60 : 0;
+
+    hours.textContent = countHours < 10 ? '0' + countHours : countHours;
+    minutes.textContent = countMinutes < 10 ? '0' + countMinutes : countMinutes;
+    seconds.textContent = countSeconds < 10 ? '0' + countSeconds : countSeconds;
+}
+
+countdownExtendedTimer();
+
+timerId = setInterval(countdownExtendedTimer, 1000);
