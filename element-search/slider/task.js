e34efe0906b25b0d9729158c8671206de8a1e801
Elshina25 <102738101+Elshina25@users.noreply.github.com>
2022-10-08 01:24:22 +0300

    slider edits

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -4,28 +4,33 @@ const sliderArrowPrev = document.querySelector('.slider__arrow_prev');
 const sliderArrowNext = document.querySelector('.slider__arrow_next');
 const dots = document.querySelectorAll('.slider__dot');
 const dotsCollection = Array.from(dots);
-let count = 0;
 
+function slider(n) {
+    let activeSlideIndex = sliderItemCollection.findIndex(slide => slide.classList.contains('slider__item_active'));
+    const dotActive = dotsCollection[activeSlideIndex].classList.add('slider__dot_active');
+    let activeDotIndex = dotsCollection.findIndex(dot => dot.classList.contains('slider__dot_active'));
 
-sliderArrowNext.addEventListener('click', () => {
-    dotsCollection[count].classList.remove('slider__dot_active');
-    count = (count + 1) % sliderItemCollection.length;
-    dotsCollection[count].classList.add('slider__dot_active');
-    active();
-});
-sliderArrowPrev.addEventListener('click', () => {
-    dotsCollection[count].classList.remove('slider__dot_active');
-    count = (count + sliderItemCollection.length - 1) % sliderItemCollection.length;
-    dotsCollection[count].classList.add('slider__dot_active');
-    active();
-});
-const active = () => {
-    const slideActive = document.querySelector('.slider__item_active');
-    if (slideActive) {
-        slideActive.classList.remove('slider__item_active');
-        sliderItemCollection[count].classList.add('slider__item_active');
-      
+    sliderItemCollection[activeSlideIndex].classList.remove('slider__item_active');
+    dotsCollection[activeDotIndex].classList.remove('slider__dot_active');
+
+    let num = n;
+    if (num > sliderItemCollection.length - 1) {
+        num = 0;
+    } if (num < 0) {
+        num = sliderItemCollection.length - 1;
     }
+    sliderItemCollection[num].classList.add('slider__item_active');
+    dotsCollection[num].classList.add('slider__dot_active');
 }
 
-
+sliderArrowNext.addEventListener('click', () => {
+    let sliderNumNext = sliderItemCollection.findIndex(slide => slide.classList.contains('slider__item_active'))
+    slider(sliderNumNext += 1);
+});
+sliderArrowPrev.addEventListener('click', () => {
+    let sliderNumPrev = sliderItemCollection.findIndex(slide => slide.classList.contains('slider__item_active'))
+    slider(sliderNumPrev -= 1);
+})
+dotsCollection.forEach((dot, index) => dot.addEventListener('click', () => {
+    slider(index);
+}));
