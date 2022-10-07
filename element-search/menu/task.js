4a8a076ceebb29aa781206d0c6cb64118b185a8b
Elshina25 <102738101+Elshina25@users.noreply.github.com>
2022-10-08 01:20:59 +0300

    menu edits

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,21 +1,22 @@
-const menuActive = event => {
-    const menuSub = document.querySelectorAll('.menu_sub');
-    const menuSubCollection = Array.from(menuSub);
-    const target = event.target.nextElementSibling;
-    target.classList.toggle('menu_active');
-    event.preventDefault();
+const menuItem = document.querySelectorAll('.menu__item');
+const menuItemArray = Array.from(menuItem);
 
-    menuSubCollection.forEach(menu => {
-        if (menu !== target) {
-            menu.classList.remove('menu_active');
+const menuLink = document.querySelectorAll('.menu__link');
+const menuLinkArray = Array.from(menuLink);
+
+menuLinkArray.forEach(link => link.addEventListener('click', function(event) {
+    const parent = this.closest('.menu_main');
+    const menuSub = parent.querySelectorAll('.menu_sub');
+    const menuSubArray = Array.from(menuSub);
+    for (let i = 0; i < menuSubArray.length; i++) {
+        if (this.nextElementSibling === menuSubArray[i]) {
+            menuSubArray[i].classList.toggle('menu_active');
+            event.preventDefault();
+        } else {
+            menuSubArray[i].classList.remove('menu_active');
         }
-    })
-  }
-  const menuLink = document.querySelectorAll('.menu__link');
-  const menuLinkCollection = Array.from(menuLink);
-  menuLinkCollection.forEach(link => {
-    link.addEventListener('click', menuActive);
-  })
+    }
+}))
 
 
- 
+
