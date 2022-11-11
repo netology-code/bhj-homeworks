754809fa510e38f8593018d918506d66c2093224
Elshina25 <102738101+Elshina25@users.noreply.github.com>
2022-11-11 16:10:57 +0300

    autocomplete is done

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,26 +68,17 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
+    const optionsArray = Array.from(this.input);
+    let arrayObj = [];
+    optionsArray.forEach(option => {
+      const optionObj = { 
+        value: option.value,
+        text: option.textContent,
+      };
+      arrayObj.push(optionObj);
+    });
 
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
-      }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    return arrayObj.filter(el => el.text.includes(text));
   }
 }
-
 new Autocomplete( document.querySelector( '.autocomplete' ));
