7e07bdbbd8004c6bd15be79ddc7d414cf78a2ecc
Elshina25 <102738101+Elshina25@users.noreply.github.com>
2022-11-11 16:06:10 +0300

    chat is done

---
+++ html-forms/chat/task.js
@@ -0,0 +1,46 @@
+const chatWidget = document.querySelector('.chat-widget');
+const chatWidgetArea = document.querySelector('.chat-widget__messages-container');
+const input = document.querySelector('.chat-widget__input')
+const messages = document.querySelector('.chat-widget__messages');
+const botPhrases = [
+    'Приветствую Вас!',
+    'Чем могу помочь?',
+    'Погодка сегодня классная!',
+    'Как Ваши дела?',
+    'До свидания! Ждем Вас снова!'
+];
+
+chatWidget.addEventListener('click', (e) => {
+    chatWidget.classList.add('chat-widget_active')
+})
+
+input.addEventListener('keyup', (e) => {
+    function date() {
+        return new Date().toLocaleTimeString().slice(0, -3);
+    }
+    const currentDate = date();
+    if (e.code === 'Enter' && input.value !== '') {
+        messages.innerHTML += `
+            <div class="message message_client">
+                <div class="message__time">${currentDate}</div>
+                <div class="message__text">${input.value}</div>
+            </div>
+        `;
+        input.value = '';
+
+        function bot() {
+            return botPhrases[Math.floor(Math.random() * botPhrases.length)];
+        }
+        const botAnswer = bot();
+
+        const delayAnswer = setTimeout(() => {
+            messages.innerHTML += `
+        <div class="message">
+            <div class="message__time">${currentDate}</div>
+            <div class="message__text">${botAnswer}</div>
+        </div>
+        `;
+        }, 2000);
+    }
+    
+})
