6ad0c6dc5b09477cadef3e1dad29beb18b3c3314
Archibaldbrown <bizzy_boss@mail.ru>
2024-02-10 14:11:40 +0100

    на первое ревью

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,18 +17,20 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    document.addEventListener('keydown', (event) => {
+      const pressedKey = event.key.toLowerCase();
+      const currentSymbol = this.currentSymbol.textContent.toLowerCase();
+
+      if (pressedKey === currentSymbol) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    });
   }
 
   success() {
-    if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
+    if (this.currentSymbol.classList.contains('symbol_current')) this.currentSymbol.classList.remove('symbol_current');
     this.currentSymbol.classList.add('symbol_correct');
     this.currentSymbol = this.currentSymbol.nextElementSibling;
 
@@ -54,41 +56,36 @@ class Game {
 
   setNewWord() {
     const word = this.getWord();
-
     this.renderWord(word);
   }
 
   getWord() {
     const words = [
-        'bob',
-        'awesome',
-        'netology',
-        'hello',
-        'kitty',
-        'rock',
-        'youtube',
-        'popcorn',
-        'cinema',
-        'love',
-        'javascript'
-      ],
-      index = Math.floor(Math.random() * words.length);
-
+      'bob',
+      'awesome',
+      'netology',
+      'hello',
+      'kitty',
+      'rock',
+      'youtube',
+      'popcorn',
+      'cinema',
+      'love',
+      'javascript'
+    ];
+    const index = Math.floor(Math.random() * words.length);
     return words[index];
   }
 
   renderWord(word) {
     const html = [...word]
-      .map(
-        (s, i) =>
-          `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
-      )
+      .map((s, i) => `<span class="symbol ${i === 0 ? 'symbol_current' : ''}">${s}</span>`)
       .join('');
     this.wordElement.innerHTML = html;
-
     this.currentSymbol = this.wordElement.querySelector('.symbol_current');
   }
 }
 
-new Game(document.getElementById('game'))
+new Game(document.getElementById('game'));
+
 
---
+++ event-object/tabs/task.js
@@ -0,0 +1,29 @@
+class Tabs {
+    constructor(container) {
+      this.container = container;
+      this.tabs = container.querySelectorAll('.tab');
+      this.tabContents = container.querySelectorAll('.tab__content');
+  
+      this.registerEvents();
+    }
+  
+    registerEvents() {
+      this.tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+          this.activateTab(tab);
+        });
+      });
+    }
+  
+    activateTab(selectedTab) {
+      this.tabs.forEach(tab => tab.classList.remove('tab_active'));
+      this.tabContents.forEach(content => content.classList.remove('tab__content_active'));
+  
+      selectedTab.classList.add('tab_active');
+      const index = Array.from(this.tabs).indexOf(selectedTab);
+      this.tabContents[index].classList.add('tab__content_active');
+    }
+  }
+  
+  new Tabs(document.querySelector('.tab__navigation'));
+  
