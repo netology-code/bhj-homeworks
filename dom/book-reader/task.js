cc1357aa2d551d7c180694e1de56440b197c8e7a
Archibaldbrown <bizzy_boss@mail.ru>
2024-02-10 14:20:00 +0100

    на ревью

---
+++ dom/book-reader/task.js
@@ -0,0 +1,31 @@
+class BookReader {
+    constructor() {
+      this.fontSizes = document.querySelectorAll('.font-size');
+      this.book = document.getElementById('book');
+      
+      this.fontSizes.forEach(fontSize => {
+        fontSize.addEventListener('click', () => {
+          this.setFontSize(fontSize.dataset.size);
+        });
+      });
+    }
+  
+    setFontSize(size) {
+      this.fontSizes.forEach(fontSize => {
+        fontSize.classList.remove('font-size_active');
+      });
+      
+      const newSizeElement = document.querySelector(`.font-size_${size}`);
+      newSizeElement.classList.add('font-size_active');
+      
+      this.book.classList.remove('book_fs-small', 'book_fs-big');
+      if (size === 'small') {
+        this.book.classList.add('book_fs-small');
+      } else if (size === 'big') {
+        this.book.classList.add('book_fs-big');
+      }
+    }
+  }
+  
+  new BookReader();
+  
