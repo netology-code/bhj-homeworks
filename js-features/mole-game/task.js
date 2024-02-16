2d65bfc29690b646c9064e3c8ee9a7e7a9e92bd7
Archibaldbrown <bizzy_boss@mail.ru>
2024-02-16 20:06:12 +0100

    внесены правки в крота

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,6 +1,7 @@
 let score = 0;
 let fails = 0;
 let scoreDisplay = document.getElementById('score');
+let failsDisplay = document.getElementById('fails'); // Переменная для отображения количества промахов
 
 function getHole(index) {
     return document.getElementById(`hole${index}`);
@@ -10,26 +11,24 @@ function startGame() {
     score = 0;
     fails = 0;
     scoreDisplay.textContent = score;
+    failsDisplay.textContent = fails; // Отображение количества промахов
 
     for (let i = 1; i <= 9; i++) {
         getHole(i).addEventListener('click', onHoleClick);
     }
-
-    setInterval(showMole, 1000);
 }
 
 function onHoleClick(event) {
     if (event.target.classList.contains('hole_has-mole')) {
         score++;
         scoreDisplay.textContent = score;
-
         if (score === 10) {
             alert('Вы выиграли!');
             endGame();
         }
     } else {
         fails++;
-
+        failsDisplay.textContent = fails; // Обновляем отображение количества промахов
         if (fails === 5) {
             alert('Игра завершена. Вы проиграли.');
             endGame();
@@ -37,22 +36,10 @@ function onHoleClick(event) {
     }
 }
 
-function showMole() {
-    for (let i = 1; i <= 9; i++) {
-        getHole(i).classList.remove('hole_has-mole');
-    }
-
-    const randomIndex = Math.floor(Math.random() * 9) + 1;
-    getHole(randomIndex).classList.add('hole_has-mole');
-}
-
 function endGame() {
     for (let i = 1; i <= 9; i++) {
         getHole(i).removeEventListener('click', onHoleClick);
     }
-
-    score = 0;
-    fails = 0;
 }
 
 window.onload = startGame;
