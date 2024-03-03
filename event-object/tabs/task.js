99e70259ed368c75e1bdb53365191d5b49ccabf4
Archibaldbrown <bizzy_boss@mail.ru>
2024-03-03 16:02:23 +0100

    метод activateTab находит соответствующий элемент .tab__content внутри контейнера tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -22,10 +22,11 @@ class Tabs {
 
     selectedTab.classList.add('tab_active');
     const index = Array.from(this.tabs).indexOf(selectedTab);
-    this.tabContents[index].classList.add('tab__content_active');
+    const content = this.container.querySelectorAll('.tab__content')[index];
+    if (content) {
+      content.classList.add('tab__content_active');
+    }
   }
 }
 
-new Tabs(document.querySelector('.tab__navigation'));
-
-  
+new Tabs(document.getElementById('tabs1'));
