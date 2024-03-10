5c5022c5d0bbdf89cbd386578f0243a7130810f0
Archibaldbrown <bizzy_boss@mail.ru>
2024-03-10 15:41:37 +0100

    корректировки и проверка кода

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,38 +1,41 @@
 class BookReader {
-    constructor() {
-      this.fontSizes = document.querySelectorAll('.font-size');
-      this.book = document.getElementById('book');
-      
-      this.fontSizes.forEach(fontSize => {
-        fontSize.addEventListener('click', (event) => {
-          event.preventDefault();
-          const size = fontSize.dataset.size;
-          if (size) { //  атрибут data-size
-            this.setFontSize(size);
-          }
-        });
-      });
-    }
-  
-    setFontSize(size) {
-      this.fontSizes.forEach(fontSize => {
-        fontSize.classList.remove('font-size_active');
-      });
-  
-      const newSizeElement = document.querySelector(`.font-size_${size}`);
-      if (newSizeElement) {
-        newSizeElement.classList.add('font-size_active');
-      }
-  
-      this.book.classList.remove('book_fs-small', 'book_fs-big');
-      if (size === 'small') {
-        this.book.classList.add('book_fs-small');
-      } else if (size === 'big') {
-        this.book.classList.add('book_fs-big');
-      }
-    }
+  constructor(container) {
+    this.container = container;
+    this.fontSizes = [...container.querySelectorAll('.font-size')];
+    this.book = container.querySelector('.book');
+    
+    this.registerEvents();
   }
-  
-  new BookReader();
-  
-  
+
+  registerEvents() {
+    this.fontSizes.forEach(fontSize => {
+      fontSize.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const size = fontSize.dataset.size;
+
+        if (size) { // Проверяем наличие атрибута data-size
+          this.clearActiveClasses(this.fontSizes, 'font-size_active');
+          fontSize.classList.add('font-size_active');
+
+          if (size === 'small') {
+            this.book.className = 'book book_fs-small';
+          } else if (size === 'big') {
+            this.book.className = 'book book_fs-big';
+          } else {
+            this.book.className = 'book';
+          }
+        }
+      });
+    });
+  }
+
+  clearActiveClasses(elements, className) {
+    elements.forEach(element => {
+      element.classList.remove(className);
+    });
+  }
+}
+
+new BookReader(document.querySelector('#book'));
+
