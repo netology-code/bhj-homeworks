cbd6e5cd3e44660d98a182e0b9e374cea4e44ef0
Любишов Влад <LubishovV@mik-inform.ru>
2024-07-24 14:03:02 +0300

    Задание Таймер обратного отсчёта. Повышенный уровень сложности 2

---
+++ js-features/countdown/task.js
@@ -0,0 +1,43 @@
+let timer = document.getElementById('timer');
+let downloadLink = 'https://drive.google.com/uc?export=download&id=1DeKTA4EGozpCG_Sdwp1-kZ_mQzyqLqtJ';
+const arrayTimer = Array.of('00', '00', '10');
+ 
+timer.innerText = arrayTimer[0] + ':' + arrayTimer[1] + ':' + arrayTimer[2];
+
+startTimer()
+
+function startTimer(){
+
+    timer.innerText = arrayTimer[0] + ':' + arrayTimer[1] + ':' + arrayTimer[2]
+    
+    if (arrayTimer[2] > 0){
+        arrayTimer[2]--;
+        if ((String(arrayTimer[2]).length !== 2)) {
+            arrayTimer[2] = '0' + arrayTimer[2]
+        }
+    } else {
+        arrayTimer[2] = 59;
+        if (arrayTimer[1] > 0) {
+            arrayTimer[2] = 59;
+            arrayTimer[1]--;
+            if ((String(arrayTimer[1]).length !== 2)) {
+                arrayTimer[1] = '0' + arrayTimer[1]
+            }
+        } else {
+            arrayTimer[1] = 59
+            if (arrayTimer[0] > 0) {
+                arrayTimer[1] = 59;
+                arrayTimer[0]--;
+                if ((String(arrayTimer[0]).length !== 2)) {
+                    arrayTimer[0] = '0' + arrayTimer[0]
+                }
+            } else {
+                window.location.href = downloadLink
+                alert('Вы победили в конкурсе!')
+                return;
+            } 
+        }
+    }
+
+    setTimeout(startTimer, 1000)
+}
